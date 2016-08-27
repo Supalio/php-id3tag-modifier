3298ad1df93543a7c502353cf4e2b96d08b9ada2
Supalio <keep_control_75@hotmail.com>
2016-08-27 23:52:38 +0200

    Added a home page with links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,24 +66,14 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHP ID3Tag Modifier
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="{{ url('/rename') }}">Rename files</a>
+                    <a href="{{ url('/tag') }}">Edit Tags</a>
                 </div>
             </div>
         </div>
